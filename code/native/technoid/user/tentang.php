8166ae80ffdc67b46fb0c2010ab7da716a00ed33
Agelprdpta <129038753+Agelprdpta@users.noreply.github.com>
2024-11-07 20:37:54 +0700

    Add files via upload

---
+++ code/native/technoid/user/tentang.php
@@ -0,0 +1,31 @@
+<?php
+require '../config/config.php';
+
+
+require 'templates/header.php';
+?>
+
+<div class="row bg-white border p-2 mt-5">
+    <div class="col-md ml-3">
+        <h1>Tentang Kami</h1>
+        <p class="lead">Kami menjual berbagai Sepeda
+            Kami mulai membuka toko kami pada tahun 2019 bulan Januari dimana saata itu kami hanya menjual 1 jenis produk yaitu Sepeda BMX
+            dan di pertengahan tahun 2020 kami menambahkan 2 jenis barang yaitu Sepeda Fixie dan Sepeda Gunung, dan juga
+            mejual berbagai jenis Sepeda untuk anak anak
+        </p>
+        <hr>
+        <div class="row ml-1">
+            <div class="w-100">
+                <h3><i class="fa fa-map-marker"></i>Alamat</h3>
+                <p><br>Tapos / Depok<br>Jawa Barat<br>Indonesia<br></p>
+            </div>
+            <!-- /.col-sm-4-->
+            <div class="">
+                <h3><i class="fa fa-phone"></i> Hubungi Kami</h3>
+                <p class="text-muted">Anda dapat menghubungi kami dinomer yang tertera dibawah ini</p>
+                <p><strong>+6287 812 212 333</strong></p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require 'templates/footer.php' ?>
